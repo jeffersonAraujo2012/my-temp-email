aaced75be3be87eeed0c5aacbcaae6c0b8988829
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-09-16 23:24:40 -0300

    Style applied to the mail-generation-container component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,27 @@ import { InboxComponent } from './components/inbox/inbox.component';
 import { MailCardComponent } from './components/inbox/mail-card/mail-card.component';
 import { MailDisplayContainerComponent } from './components/mail-display-container/mail-display-container.component';
 import { NotificationButtonComponent } from './components/notification-button/notification-button.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, EmailGenerationContainerComponent, InboxComponent, MailCardComponent, MailDisplayContainerComponent, NotificationButtonComponent],
+  declarations: [
+    AppComponent,
+    EmailGenerationContainerComponent,
+    InboxComponent,
+    MailCardComponent,
+    MailDisplayContainerComponent,
+    NotificationButtonComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     MatProgressSpinnerModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
