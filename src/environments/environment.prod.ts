9d0868c875b6bf8cb451b804e10b9756e9baa9ee
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-09-18 17:32:53 -0300

    External proxy using in production environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   environmentName: 'prod',
-  tempEmailApiUri: '/api',
+  tempEmailApiUri: 'https://cors-anywhere.herokuapp.com/https://dropmail.me/api/graphql/coodesh-mncoiwsfkmzcowemzxckweriofdsnm',
 };
