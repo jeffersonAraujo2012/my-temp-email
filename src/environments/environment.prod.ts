c1f63d52b2207f10eb460ce300c0e83915741ef0
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-09-18 21:41:32 -0300

    Updated: environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   environmentName: 'prod',
-  tempEmailApiUri: 'https://cors-anywhere.herokuapp.com/https://dropmail.me/api/graphql/coodesh-mncoiwsfkmzcowemzxckweriofdsnm',
+  tempEmailApiUri: 'http://localhost:8000/https://dropmail.me/api/graphql/coodesh-mncoiwsfkmzcowemzxckweriofdsnm',
 };
